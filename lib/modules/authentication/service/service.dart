10f4f4698dcb03fabf36777c2153873a55d9ccec
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-23 07:04:53 +0700

    refactor: add AuthenticationService Interface

--- lib/modules/authentication/service/service.dart
+++ lib/modules/authentication/service/service.dart
@@ -1,7 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-class AuthenticationService {
+abstract class IAuthenticationService {
+  Future<UserCredential> signInWithGoogle();
+  Future<void> signOut();
+  Stream<User?> authStateChanges();
+  Future<bool> isSignedIn();
+}
+
+class AuthenticationService implements IAuthenticationService {
   FirebaseAuth firebase;
   AuthenticationService._({required this.firebase}) : super();
 
@@ -15,11 +22,11 @@ class AuthenticationService {
   }
 
   /// Sign Out with from signed provider
-  Future<void> signOut() async {
-    await firebase.signOut();
-  }
+  @override
+  Future<void> signOut() async => await firebase.signOut();
 
   /// Sign in With Google
+  @override
   Future<UserCredential> signInWithGoogle() async {
     final GoogleSignInAccount? googleUser = await GoogleSignIn(scopes: [
       'profile',
@@ -36,12 +43,14 @@ class AuthenticationService {
   }
 
   /// Check if user is signed in
+  @override
   Future<bool> isSignedIn() async {
     User? currentUser = await firebase.currentUser;
     return currentUser != null;
   }
 
   /// Sign in silently
+  @override
   Stream<User?> authStateChanges() async* {
     yield* firebase.authStateChanges();
   }
