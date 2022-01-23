bc12ab282e99085356df1ff232bd5b53f4007ebf
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-24 04:41:39 +0700

    add: Home Bindings

--- lib/binding.dart
+++ lib/binding.dart
@@ -1,11 +1,23 @@
 import 'package:get/get.dart';
 import 'package:stock_list/modules/authentication/bloc/auth_bloc.dart';
+import 'package:stock_list/modules/home/cubit/home_tab_cubit.dart';
+import 'package:stock_list/modules/home/section/account/cubit/account_info_cubit.dart';
+import 'package:stock_list/modules/home/section/account/data/data.dart';
 
-/// [AuthenticationBinding] is a [GetX] binding that provides dependencies injection
+/// These are [GetX] binding that provides dependencies injection
 /// for the application to use.
 class AuthenticationBinding implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut<AuthBloc>(AuthBloc.new);
+  }
+}
+
+class HomeBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<HomeTabCubit>(HomeTabCubit.new);
+    Get.lazyPut<AccountInfoCubit>(AccountInfoCubit.new);
+    Get.lazyPut<AccountCredentials>(AccountCredentials.new);
   }
 }
