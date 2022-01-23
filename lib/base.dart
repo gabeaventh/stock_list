c115106bfbfcddefa54e6c0beda0d9e62fd0f58d
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-24 04:40:15 +0700

    refactor: remove default theme

--- lib/base.dart
+++ lib/base.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:stock_list/routes.dart';
+import 'package:stock_list/routes/routes.dart';
 
 /// [AppBase] is the base of the application.
 /// using [GetMaterialApp] to create the app.
@@ -13,35 +13,6 @@ class AppBase extends StatelessWidget {
       navigatorKey: Get.key,
       navigatorObservers: [GetObserver()],
       title: 'Stock List',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        backgroundColor: Colors.black54,
-        scaffoldBackgroundColor: Colors.black54,
-        textButtonTheme: TextButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: MaterialStateColor.resolveWith(
-              (states) => Colors.black54,
-            ),
-            foregroundColor: MaterialStateColor.resolveWith(
-              (states) => Colors.white,
-            ),
-            shape: MaterialStateProperty.resolveWith<OutlinedBorder>(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-        ),
-        textTheme: TextTheme(
-          button: TextStyle(
-            color: Colors.greenAccent,
-            fontWeight: FontWeight.bold,
-          ),
-          bodyText2: TextStyle(
-            color: Colors.white,
-          ),
-        ),
-      ),
 
       /// [AppRoutes.getRoutes] is the routes of the application.
       getPages: AppRoutes.getRoutes,
