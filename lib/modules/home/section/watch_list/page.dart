46a2c3392cfbe7cfdc74f71360d3fab6216301cc
Thomas Dwiatmoko <gabeaventh@gmail.com>
2022-01-24 04:45:07 +0700

    add: initiate watch list page

---
+++ lib/modules/home/section/watch_list/page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WatchListPage extends StatefulWidget {
+  const WatchListPage({Key? key}) : super(key: key);
+
+  @override
+  _WatchListPageState createState() => _WatchListPageState();
+}
+
+class _WatchListPageState extends State<WatchListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/modules/home/section/watch_list/view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WatchListView extends StatelessWidget {
+  const WatchListView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
